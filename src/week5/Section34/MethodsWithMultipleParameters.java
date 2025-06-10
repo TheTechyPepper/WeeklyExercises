20c8d2ace1f363bcdb5b65cb8c423efc80e23ca9
Peppers <PEPPET@pfizer.com>
2025-06-10 15:20:31 -0500

    Updated Codes

---
+++ src/week5/Section34/MethodsWithMultipleParameters.java
@@ -0,0 +1,13 @@
+//package week5.Section34;
+
+//public class MethodsWithMultipleParameters {
+    //public static void main(String[] args) {
+
+    //}
+
+    //public static void printUserInfo() {
+        //System.out.println("Welcome " );
+       // System.out.println("Your age is " + age);
+    //}
+//}
+
